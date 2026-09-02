3a88173fd8206e7897b1029389552f57273fa5c4
Hassan Shaikh <shaikh.hassan01@outlook.com>
2026-09-02 16:50:14 -0400

    completed the listed issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -47,8 +47,9 @@ var displayRepos = function (repos, searchTerm) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
     // create a div to hold this repo's info
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName );
 
     // create a span for the repo name
     var titleEl = document.createElement("span");
--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,8 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var displayIssues = function(issues) {
+var displayIssues = function (issues) {
   if (issues.length === 0) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
@@ -29,34 +30,48 @@ var displayIssues = function(issues) {
   }
 };
 
-var getRepoIssues = function(repo) {
+var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      response.json().then(function(data) {
+      response.json().then(function (data) {
         displayIssues(data);
         // check if there are more than 30 issues
         if (response.headers.get("Link")) {
-            displayWarning(repo);
+          displayWarning(repo);
         }
       });
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
 
-var displayWarning = function(repo) {
-    // add text message
-    limitWarningEl.textContent = "To see more than 30 issues, visit";
-    //create a link to github
-    var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on GitHub.com";
-    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-    linkEl.setAttribute("target", "_blank");
-    // append link to the warning container
-    limitWarningEl.appendChild(linkEl);
-}
+var displayWarning = function (repo) {
+  // add text message
+  limitWarningEl.textContent = "To see more than 30 issues, visit";
+  //create a link to github
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+  // append link to the warning container
+  limitWarningEl.appendChild(linkEl);
+};
 
-getRepoIssues("facebook/react");
+var getRepoName = function () {
+  // get the query string from the URL (?repo=HazShk/Ansible)
+  var queryString = document.location.search;
+  // split at the = sign and grab the second piece
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues();
+  } else {
+    //no repo name in url, send user back to homepage
+    document.location.replace("./indext.html");
+  }
+};
+
+getRepoName();
