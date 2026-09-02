e1fb32e98ea613bf3e06e58716a011f2a1c7f26a
Hassan Shaikh <shaikh.hassan01@outlook.com>
2026-09-02 15:09:11 -0400

    Added the code for displaying single repo and issues count

---
+++ assets/js/single.js
@@ -0,0 +1,62 @@
+var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayIssues = function(issues) {
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  for (var i = 0; i < issues.length; i++) {
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = issues[i].title;
+    issueEl.appendChild(titleEl);
+
+    var typeEl = document.createElement("span");
+    if (issues[i].pull_request) {
+      typeEl.textContent = "(Pull Request)";
+    } else {
+      typeEl.textContent = "(Issue)";
+    }
+    issueEl.appendChild(typeEl);
+
+    issueContainerEl.appendChild(issueEl);
+  }
+};
+
+var getRepoIssues = function(repo) {
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+        // check if there are more than 30 issues
+        if (response.headers.get("Link")) {
+            displayWarning(repo);
+        }
+      });
+    } else {
+      alert("There was a problem with your request!");
+    }
+  });
+};
+
+var displayWarning = function(repo) {
+    // add text message
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+    //create a link to github
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // append link to the warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
